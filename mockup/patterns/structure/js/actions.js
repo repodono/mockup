b2148a9f30b61fe48ab444011f6bb5ec5cdf9321
Tommy Yu <tommy.yu@auckland.ac.nz>
2016-01-11 15:33:52 +1300

    Initial split of Actions from ActionMenu
    - Per-item action methods should not be defined directly within the view
      class; view class should only deal with rendering/registration of the
      events, and those events should be registered within a seaparate model
      which this view class should make use of
    - Baby steps: want to make it possible to hook in events that could be
      defined by other libraries.

---
+++ mockup/patterns/structure/js/actions.js
@@ -0,0 +1,162 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'mockup-ui-url/views/base',
+  'mockup-patterns-structure-url/js/models/result',
+  'mockup-utils',
+  'translate',
+  'bootstrap-dropdown'
+], function($, _, Backbone, BaseView, Result, utils, _t) {
+  'use strict';
+
+  /*
+    Moved out of Actions to the root of this closure.
+
+    Javascript's this/self references don't always behave as one might
+    expect - calling the instantiated callable "object" directly within
+    an "instance object" might not actually "propagate" the `this`
+    reference as expected.  Though it does have closures, so rather use
+    that for a shared function within one.
+  */
+  var doAction = function(self, buttonName, successMsg, failMsg){
+    $.ajax({
+      url: self.app.buttons.get(buttonName).options.url,
+      data: {
+        selection: JSON.stringify([self.model.attributes.UID]),
+        folder: self.model.attributes.path,
+        _authenticator: utils.getAuthenticator()
+      },
+      dataType: 'json',
+      type: 'POST'
+    }).done(function(data){
+      if(data.status === 'success'){
+        self.app.setStatus(_t(successMsg + ' "' + self.model.attributes.Title + '"'));
+        self.app.collection.pager();
+        self.app.updateButtons();
+      }else{
+        self.app.setStatus(_t('Error ' + failMsg + ' "' + self.model.attributes.Title + '"'));
+      }
+    }); 
+  };
+
+  // use a more primative class than Backbone.Model?
+  var Actions = Backbone.Model.extend({
+    initialize: function(options) {
+      this.options = options;
+      this.app = options.app;
+      this.model = options.model;
+      this.selectedCollection = this.app.selectedCollection;
+    },
+    selectAll: function(e){
+      e.preventDefault();
+      var self = this;
+      var page = 1;
+      var count = 0;
+      var getPage = function(){
+        self.app.loading.show();
+        $.ajax({
+          url: self.app.collection.url,
+          type: 'GET',
+          dataType: 'json',
+          data: {
+            query: self.app.collection.queryParser({
+              searchPath: self.model.attributes.path
+            }),
+            batch: JSON.stringify({
+              page: page,
+              size: 100
+            }),
+            attributes: JSON.stringify(self.app.queryHelper.options.attributes)
+          }
+        }).done(function(data){
+          var items = self.app.collection.parse(data, count);
+          count += items.length;
+          _.each(items, function(item){
+            self.app.selectedCollection.add(new Result(item));
+          });
+          page += 1;
+          if(data.total > count){
+            getPage();
+          }else{
+            self.app.loading.hide();
+            self.app.tableView.render();
+          }
+        });
+      };
+      getPage();
+    },
+
+    cutClicked: function(e) {
+      e.preventDefault();
+      doAction(this, 'cut', _t('Cut'), _t('cutting'));
+    },
+    copyClicked: function(e) {
+      e.preventDefault();
+      doAction(this, 'copy', _t('Copied'), _t('copying'));
+    },
+    pasteClicked: function(e) {
+      e.preventDefault();
+      doAction(this, 'paste', _t('Pasted into'), _t('Error pasting into'));
+    },
+    moveTopClicked: function(e) {
+      e.preventDefault();
+      this.app.moveItem(this.model.attributes.id, 'top');
+    },
+    moveBottomClicked: function(e) {
+      e.preventDefault();
+      this.app.moveItem(this.model.attributes.id, 'bottom');
+    },
+    setDefaultPageClicked: function(e) {
+      e.preventDefault();
+      var self = this;
+      $.ajax({
+        url: self.app.getAjaxUrl(self.app.setDefaultPageUrl),
+        type: 'POST',
+        data: {
+          '_authenticator': $('[name="_authenticator"]').val(),
+          'id': this.model.attributes.id
+        },
+        success: function(data) {
+          self.app.ajaxSuccessResponse.apply(self.app, [data]);
+        },
+        error: function(data) {
+          self.app.ajaxErrorResponse.apply(self.app, [data]);
+        }
+      });
+    },
+    getSelectedBaseUrl: function() {
+      var self = this;
+      return self.model.attributes.getURL;
+    },
+    getWindow: function() {
+      var win = window;
+      if (win.parent !== window) {
+        win = win.parent;
+      }
+      return win;
+    },
+    openUrl: function(url) {
+      var self = this;
+      var win = self.getWindow();
+      var keyEvent = this.app.keyEvent;
+      if (keyEvent && keyEvent.ctrlKey) {
+        win.open(url);
+      } else {
+        win.location = url;
+      }
+    },
+    openClicked: function(e) {
+      e.preventDefault();
+      var self = this;
+      self.openUrl(self.getSelectedBaseUrl() + '/view');
+    },
+    editClicked: function(e) {
+      e.preventDefault();
+      var self = this;
+      self.openUrl(self.getSelectedBaseUrl() + '/edit');
+    },
+  });
+
+  return Actions;
+});
--- mockup/patterns/structure/js/views/actionmenu.js
+++ mockup/patterns/structure/js/views/actionmenu.js
@@ -5,25 +5,30 @@ define([
   'mockup-ui-url/views/base',
   'mockup-patterns-structure-url/js/models/result',
   'mockup-utils',
+  'mockup-patterns-structure-url/js/actions',
   'text!mockup-patterns-structure-url/templates/actionmenu.xml',
   'translate',
   'bootstrap-dropdown'
-], function($, _, Backbone, BaseView, Result, utils, ActionMenuTemplate, _t) {
+], function($, _, Backbone, BaseView, Result, utils, Actions, ActionMenuTemplate, _t) {
   'use strict';
 
   var ActionMenu = BaseView.extend({
     className: 'btn-group actionmenu',
     template: _.template(ActionMenuTemplate),
-    events: {
-      'click .selectAll a': 'selectAll',
-      'click .cutItem a': 'cutClicked',
-      'click .copyItem a': 'copyClicked',
-      'click .pasteItem a': 'pasteClicked',
-      'click .move-top a': 'moveTopClicked',
-      'click .move-bottom a': 'moveBottomClicked',
-      'click .set-default-page a': 'setDefaultPageClicked',
-      'click .openItem a': 'openClicked',
-      'click .editItem a': 'editClicked'
+    events: function() {
+      // figure out a way to let third-party supply their own ``define``ed
+      // functions into this thing.
+      return {
+        'click .selectAll a': this.actions.selectAll,
+        'click .cutItem a': this.actions.cutClicked,
+        'click .copyItem a': this.actions.copyClicked,
+        'click .pasteItem a': this.actions.pasteClicked,
+        'click .move-top a': this.actions.moveTopClicked,
+        'click .move-bottom a': this.actions.moveBottomClicked,
+        'click .set-default-page a': this.actions.setDefaultPageClicked,
+        'click .openItem a': this.actions.openClicked,
+        'click .editItem a': this.actions.editClicked
+      }
     },
     initialize: function(options) {
       this.options = options;
@@ -35,134 +40,7 @@ define([
       }else {
         this.canMove = true;
       }
-    },
-    selectAll: function(e){
-      e.preventDefault();
-      var self = this;
-      var page = 1;
-      var count = 0;
-      var getPage = function(){
-        self.app.loading.show();
-        $.ajax({
-          url: self.app.collection.url,
-          type: 'GET',
-          dataType: 'json',
-          data: {
-            query: self.app.collection.queryParser({
-              searchPath: self.model.attributes.path
-            }),
-            batch: JSON.stringify({
-              page: page,
-              size: 100
-            }),
-            attributes: JSON.stringify(self.app.queryHelper.options.attributes)
-          }
-        }).done(function(data){
-          var items = self.app.collection.parse(data, count);
-          count += items.length;
-          _.each(items, function(item){
-            self.app.selectedCollection.add(new Result(item));
-          });
-          page += 1;
-          if(data.total > count){
-            getPage();
-          }else{
-            self.app.loading.hide();
-            self.app.tableView.render();
-          }
-        });
-      };
-      getPage();
-    },
-    doAction: function(buttonName, successMsg, failMsg){
-      var self = this;
-      $.ajax({
-        url: self.app.buttons.get(buttonName).options.url,
-        data: {
-          selection: JSON.stringify([self.model.attributes.UID]),
-          folder: self.model.attributes.path,
-          _authenticator: utils.getAuthenticator()
-        },
-        dataType: 'json',
-        type: 'POST'
-      }).done(function(data){
-        if(data.status === 'success'){
-          self.app.setStatus(_t(successMsg + ' "' + self.model.attributes.Title + '"'));
-          self.app.collection.pager();
-          self.app.updateButtons();
-        }else{
-          self.app.setStatus(_t('Error ' + failMsg + ' "' + self.model.attributes.Title + '"'));
-        }
-      }); 
-    },
-    cutClicked: function(e) {
-      e.preventDefault();
-      this.doAction('cut', _t('Cut'), _t('cutting'));
-    },
-    copyClicked: function(e) {
-      e.preventDefault();
-      this.doAction('copy', _t('Copied'), _t('copying'));
-    },
-    pasteClicked: function(e) {
-      e.preventDefault();
-      this.doAction('paste', _t('Pasted into'), _t('Error pasting into'));
-    },
-    moveTopClicked: function(e) {
-      e.preventDefault();
-      this.app.moveItem(this.model.attributes.id, 'top');
-    },
-    moveBottomClicked: function(e) {
-      e.preventDefault();
-      this.app.moveItem(this.model.attributes.id, 'bottom');
-    },
-    setDefaultPageClicked: function(e) {
-      e.preventDefault();
-      var self = this;
-      $.ajax({
-        url: self.app.getAjaxUrl(self.app.setDefaultPageUrl),
-        type: 'POST',
-        data: {
-          '_authenticator': $('[name="_authenticator"]').val(),
-          'id': this.model.attributes.id
-        },
-        success: function(data) {
-          self.app.ajaxSuccessResponse.apply(self.app, [data]);
-        },
-        error: function(data) {
-          self.app.ajaxErrorResponse.apply(self.app, [data]);
-        }
-      });
-    },
-    getSelectedBaseUrl: function() {
-      var self = this;
-      return self.model.attributes.getURL;
-    },
-    getWindow: function() {
-      var win = window;
-      if (win.parent !== window) {
-        win = win.parent;
-      }
-      return win;
-    },
-    openUrl: function(url) {
-      var self = this;
-      var win = self.getWindow();
-      var keyEvent = this.app.keyEvent;
-      if (keyEvent && keyEvent.ctrlKey) {
-        win.open(url);
-      } else {
-        win.location = url;
-      }
-    },
-    openClicked: function(e) {
-      e.preventDefault();
-      var self = this;
-      self.openUrl(self.getSelectedBaseUrl() + '/view');
-    },
-    editClicked: function(e) {
-      e.preventDefault();
-      var self = this;
-      self.openUrl(self.getSelectedBaseUrl() + '/edit');
+      this.actions = new Actions(this);
     },
     render: function() {
       var self = this;
